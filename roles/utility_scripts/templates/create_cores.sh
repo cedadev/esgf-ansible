e397951d83c429b34a0a181e80873af1b5f1f08f
watucker <william.tucker@stfc.ac.uk>
2020-03-04 14:27:43 +0000

    Refactored non role changes to private repo

--- roles/utility_scripts/templates/create_cores.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-{% for core in ['datasets', 'files', 'aggregations'] %}
-# Create {{ core }} core
-{{ solr.path }}/bin/solr create_core -c {{ core }} -d /tmp/solr-home/mycore -p 8985
-{% endfor %}
